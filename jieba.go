b693abc61fc1018f8d77ec0ccb45916cf2573833
snowphoenix <1595763343@qq.com>
2022-03-29 21:42:02 +0800

    disable Wall

--- jieba.go
+++ jieba.go
@@ -1,7 +1,7 @@
 package gojieba
 
 /*
-#cgo CXXFLAGS: -I./deps -DLOGGING_LEVEL=LL_WARNING -O3 -Wall
+#cgo CXXFLAGS: -I./deps -DLOGGING_LEVEL=LL_WARNING -O3
 #include <stdlib.h>
 #include "jieba.h"
 */
